0b1998813e4afefd55f35e864259c970da38d7c0
irpaned <muhammadirfan2823@gmail.com>
2024-05-31 17:19:34 +0800

    Move to Prisma ORM and setup the basic of Prisma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -118,7 +118,7 @@ import express, { Request, Response } from "express";
 
             if(!thread) return res.status(500).json( {message : "Thread not found!"} )
 
-            // ini 👇 cara bacanya : kalau misalkan gaada berarti gausah di update, kalau ada baru diupdate
+            // ini 👇 cara bacanya : kalau misalkan gaada berarti gausah di update, kalau ada baru diupdate test
             if (dto.content) { 
                 thread.content = dto.content;
             }
